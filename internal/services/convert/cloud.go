71d9de6fc5da9bb67b590ce6b654a53433e5bd4d
eric2788 <tsukiko852@gmail.com>
2026-04-29 21:00:37 +0800

    chore: bump cloudconvert ffmpeg to 8.0.1

--- internal/services/convert/cloud.go
+++ internal/services/convert/cloud.go
@@ -90,6 +90,7 @@ func (c *cloudConvertManager) Enqueue(inputPath, outputPath, format string, dele
 			Input:   importTaskName,
 			Engine:  "ffmpeg",
 			Command: "ffmpeg",
+			EngineVersion: "8.0.1",
 			Arguments: fmt.Sprintf(
 				"-i \"/input/%s/%s\" -map 0 -map_metadata 0 -movflags +faststart -c copy \"/output/%s\"",
 				importTaskName,
