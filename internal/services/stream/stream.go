54f66f42562600255d48cb0855f1b9b68caf21dc
eric2788 <tsukiko852@gmail.com>
2026-05-09 14:44:50 +0800

    refactor: remove redundant functions and constructor arguments

--- internal/services/stream/stream.go
+++ internal/services/stream/stream.go
@@ -1,23 +1,19 @@
 package stream
 
 import (
-	"github.com/eric2788/bilirec/internal/modules/bilibili"
 	"github.com/eric2788/bilirec/pkg/pool"
-	"github.com/go-resty/resty/v2"
 	"github.com/sirupsen/logrus"
 )
 
 var logger = logrus.WithField("service", "stream")
 
 type Service struct {
-	pool             *pool.BytesPool
-	hlsClientFactory func() *resty.Client
+	pool *pool.BytesPool
 }
 
-func NewService(bilic *bilibili.Client) *Service {
+func NewService() *Service {
 	return &Service{
-		pool:             pool.NewBytesPool(256 * 1024),
-		hlsClientFactory: bilic.NewLiveHlsClient,
+		pool: pool.NewBytesPool(256 * 1024),
 	}
 }
 
@@ -25,11 +21,4 @@ func (r *Service) Flush(buf []byte) {
 	if cap(buf) == r.pool.BufferSize {
 		r.pool.PutBytes(buf)
 	}
-}
-
-func (r *Service) newHlsClient() *resty.Client {
-	if r.hlsClientFactory != nil {
-		return r.hlsClientFactory()
-	}
-	return resty.New()
 }
